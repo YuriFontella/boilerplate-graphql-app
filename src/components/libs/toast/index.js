72440bcb263a4ce393f11eb9ad0bf0836822d60b
Yuri Fontella <yurifc4@gmail.com>
2021-06-16 11:23:22 -0300

    toast css

--- src/components/libs/toast/index.js
+++ src/components/libs/toast/index.js
@@ -1,12 +1,12 @@
-import { FireIcon } from '@heroicons/react/solid'
+import { SpeakerphoneIcon } from '@heroicons/react/solid'
 
 const Toast = ({ message }) => {
 
   return (
-    <div className="fixed flex items-end justify-center w-full bottom-2">
-      <div className="flex z-50 space-x-3 text-white text-sm p-4 rounded shadow-xl bg-indigo-500">
+    <div className="fixed flex items-end justify-center w-full bottom-2 fade-in">
+      <div className="flex z-50 space-x-3 text-white text-sm p-3.5 rounded shadow-xl bg-indigo-500">
         <span>
-          <FireIcon className="h-5 w-5" />
+          <SpeakerphoneIcon className="h-5 w-5" />
         </span>
         <span>
           <p className="font-medium">{message}</p>
